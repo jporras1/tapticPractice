495a9058f9a719327ae6135e5b9ca0f2c930d0d0
jporras1 <javi.jr2150@gmail.com>
2019-10-21 15:54:58 -0500

    Initial Commit

---
+++ tapticPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapticPractice
+//
+//  Created by Javier Porras jr on 10/21/19.
+//  Copyright © 2019 Javier Porras jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
